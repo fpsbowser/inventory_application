0ea9c07395e0242ef8be49280b00472db28054f5
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-10-28 18:52:22 -0400

    added helmet and compression middleware

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const mongoDB = require("./secure");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var catalogRouter = require("./routes/catalog");
+const compression = require("compression");
+const helmet = require("helmet");
 
 var app = express();
 
@@ -24,6 +26,8 @@ db.on("open", () => {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(compression());
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
